7bff4867751b50dc742443ac04b704efa136dd6e
muyi <yfdezhuye@163.com>
2014-10-30 14:14:18 +0800

    freb cmd

--- test/all.js
+++ test/all.js
@@ -1,3 +1,12 @@
 /**
  * Created by yang.fei on 2014/10/30.
  */
+var freb = require('../lib/freb.js');
+var path = require('path');
+var fs = require('fs');
+
+var dir = path.dirname(fs.realpathSync(__filename));
+
+console.log(dir);
+
+freb.lower(dir + '/data/test.txt');
